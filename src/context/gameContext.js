b5821907876f8730b9ae8078beb9009864a959e8
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-04 09:24:33 +0100

    Added a context provider for easier testing of app

--- src/context/gameContext.js
+++ src/context/gameContext.js
@@ -14,8 +14,21 @@ const GameProvider = ({ children }) => {
     </GameContext.Provider>
   )
 }
+
+// used for testing only
+export const GameProviderValueByProp  = ({ children, defaultState}) => {
+    const [state, dispatch] =  useReducer(gameReducer, defaultState)
+    return (
+      <GameContext.Provider value={{ state, dispatch }}>
+        {children}
+      </GameContext.Provider>
+    )
+  }
+
 GameProvider.propTypes = {
   children: element.isRequired
 }
+
+
 
 export default GameProvider
