f96f4872c22a756a52ce370a0a7ddba69f11018f
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-10-04 09:25:33 +0100

    Added more game panel tests

--- src/components/game-panel.test.js
+++ src/components/game-panel.test.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { waitForElement, render, fireEvent } from '@testing-library/react'
-import GameProvider from '../context/gameContext'
+import { GameProviderValueByProp } from '../context/gameContext'
 import GamePanel from './game-panel'
 import Copy from '../utils/copy'
+import { PLAYERS_LOADED } from '../reducer/types'
 
 const {
   labels: { buttonLabel, hintLabel }
 } = Copy
 
+const players = [
+  {
+    id: '1',
+    first_name: 'Joe',
+    last_name: 'Blogs',
+    images: { default: { width: 200, height: 200, url: 'player-one-url' } }
+  },
+  {
+    id: '2',
+    first_name: 'Jane',
+    last_name: 'Blogs',
+    images: { default: { width: 200, height: 200, url: 'player-two-url' } }
+  }
+]
+const defaultState = {
+  playing: false,
+  players,
+  playerScore: 0,
+  seenPlayersIds: [],
+  nextPair: players,
+  gameStatus: PLAYERS_LOADED
+}
+
 describe('<GamePanel/>', () => {
   let wrapper
   beforeEach(() => {
     wrapper = render(
-      <GameProvider>
+      <GameProviderValueByProp defaultState={defaultState}>
         <GamePanel />
-      </GameProvider>
+      </GameProviderValueByProp>
     )
   })
 
@@ -30,10 +54,16 @@ describe('<GamePanel/>', () => {
   })
 
   it('should have an onClick function that starts the game', async () => {
-    const { getByText } = wrapper
+    const { getByText, getByTestId, getByAltText } = wrapper
     const gameBtn = getByText(buttonLabel)
     fireEvent.click(gameBtn)
     await waitForElement(() => getByText(hintLabel))
     expect(getByText(hintLabel)).toBeDefined()
+    expect(getByTestId('player-tile-1')).toBeDefined()
+    expect(getByTestId('player-tile-2')).toBeDefined()
+    expect(getByText('Joe Blogs')).toBeDefined()
+    expect(getByText('Jane Blogs')).toBeDefined()
+    expect(getByAltText('image of Joe Blogs')).toBeDefined()
+    expect(getByAltText('image of Jane Blogs')).toBeDefined()
   })
 })
